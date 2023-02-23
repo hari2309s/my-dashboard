a6a1d15993625ae74512d79cc642cbf416694f09
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2023-02-23 22:46:38 +0530

    store setup for host application

---
+++ dashboard/src/store/store.ts
@@ -0,0 +1,18 @@
+import {
+  combineReducers,
+  configureStore,
+  PreloadedState,
+} from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({});
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
